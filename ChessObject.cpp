2248313a8c824f15bcfbf0cfbd807c066b7e5774
NateGaNe <81611522+NateGaNe@users.noreply.github.com>
2021-05-16 23:09:13 -0700

    Create ChessObject.cpp

---
+++ ChessObject.cpp
@@ -0,0 +1,13 @@
+#include "ChessObject.h"
+
+ChessObject::ChessObject(char c) {
+
+}
+
+char ChessObject::getColor() const {
+  return color;
+}
+
+char ChessObject::getPieceName() const {
+  return pieceName;
+}
