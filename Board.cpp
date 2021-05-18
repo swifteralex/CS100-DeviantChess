3d4b6986985029ea324ebfd9f8b0764df46a8b49
Devotrix <jchan443@ucr.edu>
2021-05-18 14:19:42 -0700

    fixed some old habits

--- Board.cpp
+++ Board.cpp
@@ -27,31 +27,31 @@ vector<int> findVPos(string &pos){
     sstream g(num);
     int n = 0;
     g >> n;
-    posit(0) = n;
+    posit.at(0) = n;
     string post = pos.substr(0,1);
     if(post == 'a'|| post == 'A'){
-        posit[1] = 0;
+        posit.at(1) = 0;
     }
     else(post == "b" || post == "B"){
-        posit[1] = 1;
+        posit.at(1) = 1;
     }
     else(post == "c" || post == "C"){
-        posit[1] = 2;
+        posit.at(1) = 2;
     }
     else(post == "d" || post == "D"){
-        posit[1] = 3;
+        posit.at(1) = 3;
     }
     else(post == "e" || post == "E"){
-        posit[1] = 4;
+        posit.at(1) = 4;
     }
     else(post == "f" || post == "F"){
-        posit[1] = 5;
+        posit.at(1) = 5;
     }
     else(post == "g" || post == "G"){
-        posit[1] = 6;
+        posit.at(1) = 6;
     }
     else(post == "h" || post == "H"){
-        posit[1] = 7;
+        posit.at(1) = 7;
     }
     return posit;
 }
