40e4ff24baca7991c35d68844821028e2aeeaaac
swifteralex <swifteralexc@gmail.com>
2021-05-25 18:10:10 -0700

    Removed playMove() function from Piece classes

--- Piece.cpp
+++ Piece.cpp
@@ -4,19 +4,11 @@ Piece::Piece(Board* b, char c, const std::string& l) : ChessObject(c, l), chessB
 
 Pawn::Pawn(Board* b, char c, const std::string& l) : Piece(b, c, l) {}
 
-bool Pawn::playMove(const std::string& move) {
-    return true;
-}
-
 std::vector<std::string>* Pawn::getLegalMoves() {
     return new std::vector<std::string>();
 }
 
 Knight::Knight(Board* b, char c, const std::string& l) : Piece(b, c, l) {}
-
-bool Knight::playMove(const std::string& move) {
-    return true;
-}
 
 std::vector<std::string>* Knight::getLegalMoves() {
     return new std::vector<std::string>();
@@ -24,19 +16,11 @@ std::vector<std::string>* Knight::getLegalMoves() {
 
 Bishop::Bishop(Board* b, char c, const std::string& l) : Piece(b, c, l) {}
 
-bool Bishop::playMove(const std::string& move) {
-    return true;
-}
-
 std::vector<std::string>* Bishop::getLegalMoves() {
     return new std::vector<std::string>();
 }
 
 Rook::Rook(Board* b, char c, const std::string& l) : Piece(b, c, l) {}
-
-bool Rook::playMove(const std::string& move) {
-    return true;
-}
 
 std::vector<std::string>* Rook::getLegalMoves() {
     return new std::vector<std::string>();
@@ -44,19 +28,11 @@ std::vector<std::string>* Rook::getLegalMoves() {
 
 Queen::Queen(Board* b, char c, const std::string& l) : Piece(b, c, l) {}
 
-bool Queen::playMove(const std::string& move) {
-    return true;
-}
-
 std::vector<std::string>* Queen::getLegalMoves() {
     return new std::vector<std::string>();
 }
 
 King::King(Board* b, char c, const std::string& l) : Piece(b, c, l) {}
-
-bool King::playMove(const std::string& move) {
-    return true;
-}
 
 std::vector<std::string>* King::getLegalMoves() {
     return new std::vector<std::string>();
--- Piece.h
+++ Piece.h
@@ -13,49 +13,42 @@ private:
   
 public:
     Piece(Board*, char, const std::string&);
-    virtual bool playMove(const std::string&) = 0;
     virtual std::vector<std::string>* getLegalMoves() = 0;
 };
 
 class Pawn : public Piece {
 public:
     Pawn(Board*, char, const std::string&);
-    bool playMove(const std::string&);
     std::vector<std::string>* getLegalMoves();
 };
 
 class Knight : public Piece {
 public:
     Knight(Board*, char, const std::string&);
-    bool playMove(const std::string&);
     std::vector<std::string>* getLegalMoves();
 };
 
 class Bishop : public Piece {
 public:
     Bishop(Board*, char, const std::string&);
-    bool playMove(const std::string&);
     std::vector<std::string>* getLegalMoves();
 };
 
 class Rook : public Piece {
 public:
     Rook(Board*, char, const std::string&);
-    bool playMove(const std::string&);
     std::vector<std::string>* getLegalMoves();
 };
 
 class Queen : public Piece {
 public:
     Queen(Board*, char, const std::string&);
-    bool playMove(const std::string&);
     std::vector<std::string>* getLegalMoves();
 };
 
 class King : public Piece {
 public:
     King(Board*, char, const std::string&);
-    bool playMove(const std::string&);
     std::vector<std::string>* getLegalMoves();
 };
 
