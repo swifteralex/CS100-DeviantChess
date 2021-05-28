fc034f89951b218d5e582716d32f783bb40f4ff6
swifteralex <54672346+swifteralex@users.noreply.github.com>
2021-05-28 14:13:09 -0700

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -2,6 +2,7 @@
 #include "is_in_check_test.hpp"
 #include "get_legal_moves_test.hpp"
 #include "generate_FEN_test.hpp"
+#include "checkmate_stalemate_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
