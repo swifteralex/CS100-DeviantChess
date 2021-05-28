021e58f40771882f642d9ceab32fb016a9edea12
NateGaNe <81611522+NateGaNe@users.noreply.github.com>
2021-05-27 20:59:38 -0700

    Simple main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include "Board.h"
+#include <iostream>
 
 int main() {
     Board board;
-
-    board.printBoard();
+    
+    while (!board.isCheckmated() && !board.isStalemated()) {
+        string userMove1 = "";
+        string userMove2 = "";
+        board.printBoard();
+        std::cout << "Enter the location of the piece you want to move." << std::endl;
+        cin >> userMove1;
+        std::cout << "Enter the location of where you want to move." << std::endl;
+        cin >> userMove2;
+        board.updateBoard(userMove1, userMove2);
+    }
 
     return 0;
 }
