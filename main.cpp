f0b14c156879f05f5cba33a7a02751d3f71e03d6
DevoTrix <jchan443@ucr.edu>
2021-05-28 19:43:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,22 @@
 #include "Board.h"
 
 int main() {
-    Board board;
-     std::vector<std::vector<char>> sp = {
-        { '0', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
-        { 'P', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
+    std::vector<std::vector<char>> sp = {
+        { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
+        { 'p', '0', 'p', 'p', 'p', 'p', 'p', 'p' },
         { '0', '0', '0', '0', '0', '0', '0', '0' },
-        { '0', '0', '0', '0', '0', '0', '0', '0' },
-        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { '0', 'p', '0', '0', '0', '0', '0', '0' },
+        { 'P', '0', '0', '0', '0', '0', '0', '0' },
         { '0', '0', '0', '0', '0', '0', '0', '0' },
         { '0', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
-        { 'R', 'N', 'B', '0', 'K', 'B', 'N', 'R' }
+        { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' }
     };
+    Board board;
     board.setPosition(sp);
     board.setColor('w');
-
-    Piece* p = board.getPieceAt("a7");
-    std::vector<std::string> m = p->getLegalMoves();
-    for(int i = 0; i < m.size(); i++){
-        std::cout << m[i] << std::endl;
-    }
-    if(board.updateBoard("a7", "b8n")){
+    // board.updateBoard("a4", "b5");
+    // EXPECT_EQ(board.printBoard(), ans);
+    if(board.updateBoard("a4", "b5")){
         std::cout << board.printBoard();
     }
     else{
