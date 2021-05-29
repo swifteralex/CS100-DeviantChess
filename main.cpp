e5ffbce7cf0f7938178e790fca39d73eb1525eb7
DevoTrix <jchan443@ucr.edu>
2021-05-28 18:48:49 -0700

    testing logic

--- main.cpp
+++ main.cpp
@@ -2,8 +2,22 @@
 
 int main() {
     Board board;
-
-    std::cout << board.printBoard();
-
+     std::vector<std::vector<char>> sp = {
+        { '0', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
+        { 'P', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
+        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { '0', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
+        { 'R', 'N', 'B', '0', 'K', 'B', 'N', 'R' }
+    };
+    board.setPosition(sp);
+    board.setColor('w');
+    if(board.updateBoard("a7", "a8q")){
+        std::cout << board.printBoard();
+    }
+    
+    
     return 0;
 }
