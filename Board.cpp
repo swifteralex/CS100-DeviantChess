8688d3df251587ba8d740288f60748c53669470b
DevoTrix <71916304+DevoTrix@users.noreply.github.com>
2021-05-29 03:36:26 -0700

    one more

--- Board.cpp
+++ Board.cpp
@@ -242,16 +242,16 @@ bool Board::updateBoard(std::string pos1, std::string pos2c){
     }
     if(!promo && (currentPiece->getLabel() == "p")){
         if(pos2v[0] == 0 || pos2v[0] == 7){
-            pos[pos1v[0]][pos1v[1]] = pos[pos2v[0]][pos2v[1]];
-            pos[pos2v[0]][pos2v[1]] = new Pawn(this, 'b', "p");
+            pos[pos1v[0]][pos1v[1]] = new Pawn(this, 'b', "p");
+            pos[pos2v[0]][pos2v[1]] = prev;
             // std::cout << "King is in Check. Invalid Move" << std::endl;
             return false;
         }
     }
     if(!promo && (currentPiece->getLabel() == "P")){
         if(pos2v[0] == 0 || pos2v[0] == 7){
-            pos[pos1v[0]][pos1v[1]] = pos[pos2v[0]][pos2v[1]];
-            pos[pos2v[0]][pos2v[1]] = new Pawn(this, 'w', "P");
+            pos[pos1v[0]][pos1v[1]] = new Pawn(this, 'w', "P");
+            pos[pos2v[0]][pos2v[1]] = prev;
             // std::cout << "King is in Check. Invalid Move" << std::endl;
             return false;
         }
