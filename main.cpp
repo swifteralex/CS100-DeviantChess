221c88e57cac74e8df331a221dfbce91204d1f62
DevoTrix <71916304+DevoTrix@users.noreply.github.com>
2021-05-29 04:14:19 -0700

    more debugging

--- main.cpp
+++ main.cpp
@@ -1,27 +1,24 @@
 #include "Board.h"
 
 int main() {
-    std::vector<std::vector<char>> sp = {
-        { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
-        { 'p', '0', 'p', 'p', 'p', 'p', 'p', 'p' },
+    std::vector<std::vector<char>> pos = {
+        { '0', '0', 'b', 'q', 'k', 'b', 'n', 'r' },
+        { 'p', 'p', 'p', 'p', 'p', '0', '0', 'p' },
         { '0', '0', '0', '0', '0', '0', '0', '0' },
-        { '0', 'p', '0', '0', '0', '0', '0', '0' },
-        { 'P', '0', '0', '0', '0', '0', '0', '0' },
+        { '0', '0', '0', '0', '0', '0', 'p', 'Q' },
         { '0', '0', '0', '0', '0', '0', '0', '0' },
-        { '0', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
-        { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' }
+        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
+        { '0', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' }
     };
     Board board;
-    board.setPosition(sp);
-    board.setColor('w');
-    // board.updateBoard("a4", "b5");
-    // EXPECT_EQ(board.printBoard(), ans);
-    if(board.updateBoard("a4", "b5")){
-        std::cout << board.printBoard();
+    board.setPosition(pos);
+    Piece* p = board.getPieceAt("a7");
+    std::vector<std::string> moves = p->getLegalMoves();
+    std::vector<std::string> ans = { "a7a8r", "a7a8n", "a7a8b", "a7a8q"};
+    // EXPECT_EQ(moves == ans, true);
+    for(int i = 0; i < moves.size(); i++){
+        std::cout << moves[i] << std::endl;
     }
-    else{
-	std::cout << "fail"<< std::endl;
-    }
-    
     return 0;
 }
