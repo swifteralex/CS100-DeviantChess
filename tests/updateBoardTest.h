30888e6f10b7357c24f013ab52ec8489e88c2c24
DevoTrix <71916304+DevoTrix@users.noreply.github.com>
2021-05-29 03:12:26 -0700

    grouped and did castling error test

--- tests/updateBoardTest.h
+++ tests/updateBoardTest.h
@@ -4,7 +4,7 @@
 #include "gtest/gtest.h"
 #include "../Board.h"
 
-TEST(updateBoardTest, pawnMove){
+TEST(pawnTest, pawnMove){
     std::string ans =  "   =====================================\n8  |   r   n   b   q   k   b   n   r   |\n   |                                   |\n7  |   p   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   P   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   0   P   P   P   P   P   P   P   |\n   |                                   |\n1  |   R   N   B   Q   K   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     Board board;
     board.setColor('w');
@@ -12,7 +12,7 @@ TEST(updateBoardTest, pawnMove){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, knightMove){
+TEST(knightTest, knightMove){
     std::string ans =  "   =====================================\n8  |   r   n   b   q   k   b   n   r   |\n   |                                   |\n7  |   p   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   N   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   P   P   P   P   P   P   P   P   |\n   |                                   |\n1  |   R   0   B   Q   K   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     Board board;
     board.setColor('w');
@@ -20,7 +20,7 @@ TEST(updateBoardTest, knightMove){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, rookMoveEatsPawn){
+TEST(rookTest, rookMoveEatsPawn){
     std::string ans =  "   =====================================\n8  |   r   n   b   q   k   b   n   r   |\n   |                                   |\n7  |   R   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   0   P   P   P   P   P   P   P   |\n   |                                   |\n1  |   0   N   B   Q   K   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
@@ -39,7 +39,7 @@ TEST(updateBoardTest, rookMoveEatsPawn){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, bishopMove){
+TEST(bishopTest, bishopMove){
     std::string ans =  "   =====================================\n8  |   r   n   b   q   k   b   n   r   |\n   |                                   |\n7  |   p   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   B   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   P   0   P   P   P   P   P   P   |\n   |                                   |\n1  |   R   N   0   Q   K   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
@@ -58,7 +58,7 @@ TEST(updateBoardTest, bishopMove){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, queenMove){
+TEST(queenTest, queenMove){
     std::string ans =  "   =====================================\n8  |   r   n   b   q   k   b   n   r   |\n   |                                   |\n7  |   p   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   Q   0   0   0   0   0   0   |\n   |                                   |\n2  |   P   P   0   P   P   P   P   P   |\n   |                                   |\n1  |   R   N   B   0   K   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
@@ -77,7 +77,7 @@ TEST(updateBoardTest, queenMove){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, kingMove){
+TEST(kingTest, kingMove){
     std::string ans = "   =====================================\n8  |   r   n   b   q   k   b   n   r   |\n   |                                   |\n7  |   p   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   P   P   P   P   P   P   P   P   |\n   |                                   |\n1  |   R   N   B   K   0   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
@@ -96,7 +96,7 @@ TEST(updateBoardTest, kingMove){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest,pawnEatpawn){
+TEST(pawnTest,pawnEatpawn){
     std::string ans =  "   =====================================\n8  |   r   n   b   q   k   b   n   r   |\n   |                                   |\n7  |   p   0   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   P   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   0   P   P   P   P   P   P   P   |\n   |                                   |\n1  |   R   N   B   Q   K   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
@@ -115,7 +115,7 @@ TEST(updateBoardTest,pawnEatpawn){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, pawnPromo){
+TEST(pawnTest, pawnPromo){
     std::string ans ="   =====================================\n8  |   Q   n   b   q   k   b   n   r   |\n   |                                   |\n7  |   0   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   0   P   P   P   P   P   P   P   |\n   |                                   |\n1  |   R   N   B   0   K   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { '0', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
@@ -134,7 +134,7 @@ TEST(updateBoardTest, pawnPromo){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, castlingQueenSide){
+TEST(castlingTest, castlingQueenSide){
     std::string ans ="   =====================================\n8  |   0   0   k   r   0   b   n   r   |\n   |                                   |\n7  |   p   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   P   P   P   P   P   P   P   P   |\n   |                                   |\n1  |   R   N   B   Q   K   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { 'r', '0', '0', '0', 'k', 'b', 'n', 'r' },
@@ -153,7 +153,7 @@ TEST(updateBoardTest, castlingQueenSide){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, castlingKingSide){
+TEST(castlingTest, castlingKingSide){
     std::string ans ="   =====================================\n8  |   r   n   b   q   0   r   k   0   |\n   |                                   |\n7  |   p   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   P   P   P   P   P   P   P   P   |\n   |                                   |\n1  |   R   N   B   Q   K   B   N   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { 'r', 'n', 'b', 'q', 'k', '0', '0', 'r' },
@@ -172,7 +172,7 @@ TEST(updateBoardTest, castlingKingSide){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, rookHorizontal){
+TEST(rookTest, rookHorizontal){
     std::string ans ="   =====================================\n8  |   r   n   b   q   k   b   n   r   |\n   |                                   |\n7  |   p   p   p   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   P   P   P   P   K   P   P   P   |\n   |                                   |\n1  |   0   0   0   0   0   0   R   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
@@ -191,7 +191,7 @@ TEST(updateBoardTest, rookHorizontal){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest, QueenHorizontalVertical){
+TEST(queenTest, QueenHorizontalVertical){
     std::string ans ="   =====================================\n8  |   r   n   Q   q   k   b   n   r   |\n   |                                   |\n7  |   p   p   0   p   p   p   p   p   |\n   |                                   |\n6  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n5  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n4  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n3  |   0   0   0   0   0   0   0   0   |\n   |                                   |\n2  |   P   P   0   P   K   P   P   P   |\n   |                                   |\n1  |   0   0   0   0   0   0   0   R   |\n   |                                   |\n   =====================================\n       A   B   C   D   E   F   G   H\n";
     std::vector<std::vector<char>> sp = {
         { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
@@ -211,23 +211,40 @@ TEST(updateBoardTest, QueenHorizontalVertical){
     EXPECT_EQ(board.printBoard(), ans);
 }
 
-TEST(updateBoardTest,invalidMove){
+TEST(errorTest,invalidMove){
     Board board;
     board.setColor('w');
     EXPECT_EQ(board.updateBoard("a1","a5") == false, true);
 }
 
-TEST(updateBoardTest, wrongColor){
+TEST(erroTest, wrongColor){
     Board board;
     board.setColor('b');
     EXPECT_EQ(board.updateBoard("a2", "a4") == false, true);
 }
 
-TEST(updateBoardTest, pawnOffSpawn){
+TEST(pawnTest, pawnOffSpawn){
     Board board;
     board.setColor('w');
     board.updateBoard("a2", "a4");
     EXPECT_EQ(board.updateBoard("a4","a6"), false);
 }
-
+TEST(castlingTest, noCastling){
+    std::vector<std::vector<char>> sp = {
+        { 'r', 'n', 'b', 'q', 'k', '0', '0', 'r' },
+        { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
+        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { '0', '0', '0', '0', '0', '0', '0', '0' },
+        { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
+        { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' }
+    };
+    Board board;
+    board.setCastling("-")
+    board.setPosition(sp);
+    board.setColor('b');
+    // board.updateBoard("e8", "g8");
+    EXPECT_EQ(board.updateBoard("e8", "g8"), false);
+}
 #endif
