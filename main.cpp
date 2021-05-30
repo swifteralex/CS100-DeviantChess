e9f7e62b0aefe59282d6022de246db004cfa702a
NateGaNe <81611522+NateGaNe@users.noreply.github.com>
2021-05-29 22:04:52 -0700

    Added color pickers

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main() {
 	bool playAgain = true;
 	while (playAgain) {
                 Board board;
+		char userColor;
                 char userInput;
 		char secondInput;
 		cout << "Enter 1 to play against a player, 2 to play against Stockfish (Chess Engine), or 3 to play against a bot with random moves." << endl;
@@ -15,6 +16,14 @@ int main() {
 		while (userInput != '1' && userInput != '2' && userInput != '3' || !isdigit(userInput)) {
 			cout << "Invalid input, try again." << endl;
 			cin >> userInput;
+		}
+		if (userInput == '2' || userInput == '3') {
+			cout << "Which color would you like to play as? Enter W for white or B for black. << endl;
+			cin >> userColor;
+			while (userColor != 'B' && userColor != 'W') {
+				cout << "Invalid color, try again. << endl;
+				cin >> userColor;
+			}
 		}
 		
 		if (userInput == '1') {
@@ -79,30 +88,60 @@ int main() {
 				AI = new Random(&board);
 			}
 			board.setStrategy(AI);
-               		while (!board.isCheckmated() && !board.isStalemated()){
-                		string userMove1 = "";
-                       		string userMove2 = "";
-                        	while (1) {
-                	        	userMove1 = "";
-                        		userMove2 = "";
-                              		std::cout << board.printBoard() << std::endl;
-                              		cout << "Enter the location of the piece you want to move." << endl;
-                           		cin >> userMove1;
-                           		cout << "Enter the location of where you want to move." << endl;
-                           		cin >> userMove2;
-                                	if (board.updateBoard(userMove1, userMove2)) {
+			
+			if (userColor == 'W') {
+				while (!board.isCheckmated() && !board.isStalemated()){
+					string userMove1 = "";
+					string userMove2 = "";
+					while (1) {
+						userMove1 = "";
+						userMove2 = "";
+						std::cout << board.printBoard() << std::endl;
+						cout << "Enter the location of the piece you want to move." << endl;
+						cin >> userMove1;
+						cout << "Enter the location of where you want to move." << endl;
+						cin >> userMove2;
+						if (board.updateBoard(userMove1, userMove2)) {
+							break;
+						} else {
+							cout << "Invalid move, input move again." << endl;
+						}
+					} 
+					board.setColor('b');
+					if (board.isCheckmated() || board.isStalemated()) {
 						break;
-					} else {
-						cout << "Invalid move, input move again." << endl;
 					}
-				} 
-				board.setColor('b');
-                        	if (board.isCheckmated() || board.isStalemated()) {
-					break;
+					board.playEngineMove();
+					board.setColor('w');
 				}
-                        	board.playEngineMove();
-				board.setColor('w');
-			}	
+			}
+			
+			else if (userColor == 'B') {
+				board.playEngineMove();
+				board.setColor('b');
+				while (!board.isCheckmated() && !board.isStalemated()){
+					string userMove1 = "";
+					string userMove2 = "";
+					while (1) {
+						userMove1 = "";
+						userMove2 = "";
+						std::cout << board.printBoard() << std::endl;
+						cout << "Enter the location of the piece you want to move." << endl;
+						cin >> userMove1;
+						cout << "Enter the location of where you want to move." << endl;
+						cin >> userMove2;
+						if (board.updateBoard(userMove1, userMove2)) {
+							break;
+						} else {
+							cout << "Invalid move, input move again." << endl;
+						}
+					} 
+					board.setColor('w');
+					if (board.isCheckmated() || board.isStalemated()) {
+						break;
+					}
+				}
+			}
 		} else {
 			cout << "Error with userInput" << endl;
 			return 1;
